f1e58bb9c8ef2e92bb9cc95212a65f3c8e58a090
Sam Wetzel <samwow13@yahoo.com>
2018-03-08 12:30:15 -0800

    DB can now output to screen

--- app/src/main/java/com/example/sam/hikingoregon/MainActivity.java
+++ app/src/main/java/com/example/sam/hikingoregon/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.sam.hikingoregon;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDB;
     EditText editHike, editDescription, editLength;
     Button btnAddData;
+    Button btnViewAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
         editDescription = (EditText)findViewById(R.id.editText_description);
         editLength = (EditText)findViewById(R.id.editText_length);
         btnAddData = (Button)findViewById(R.id.button_add);
+        btnViewAll = (Button)findViewById(R.id.button_view);
         AddData();
+        viewAll();
     }
 
     public void AddData(){
@@ -44,5 +49,37 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    public void viewAll() {
+        btnViewAll.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View v) {
+                    Cursor res = myDB.getAllData();
+                    if (res.getCount() == 0) {
+                        //show message
+                        showMessage("Error", "Nothing found");
+                        return;
+                    }
+
+                    StringBuffer buffer = new StringBuffer();
+                    while(res.moveToNext()) {
+                        buffer.append("Id : " + res.getString(0) + "\n");
+                        buffer.append("Hike : " + res.getString(1) + "\n");
+                        buffer.append("Description : " + res.getString(2) + "\n");
+                        buffer.append("Length : " + res.getString(3) + "\n\n");
+                    }
+                    showMessage("Data", buffer.toString());
+                }
+            }
+        );
+    }
+
+    public void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
