7f3907a72acd4873c3eaa0c08d431c4d827cde98
Sam Wetzel <samwow13@yahoo.com>
2018-03-08 11:18:56 -0800

    Can now add information to the Database through UI

--- app/src/main/java/com/example/sam/hikingoregon/DatabaseHelper.java
+++ app/src/main/java/com/example/sam/hikingoregon/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.sam.hikingoregon;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,6 +13,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "hikes.db";
     public static final String TABLE_NAME = "student_table";
+
+    //creating columns for DB
     public static final String COL_1 = "ID";
     public static final String COL_2 = "HIKE";
     public static final String COL_3 = "DESCRIPTION";
@@ -19,15 +22,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, HIKE TEXT, DESCRIPTION TEXT, LENGTH TEXT )"); //setting column types
+    }
 
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME); // if table is created drop table
+        onCreate(db);
+    }
+
+    /*
+    Method inserts data into the Database given the text values in the text boxes.
+     */
+    public boolean insertData(String hike, String description, String length){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, hike);
+        contentValues.put(COL_3, description);
+        contentValues.put(COL_4, length);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) {
+            return false;
+        }else{
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/sam/hikingoregon/MainActivity.java
+++ app/src/main/java/com/example/sam/hikingoregon/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.sam.hikingoregon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.widget.Toast.makeText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDB;
+    EditText editHike, editDescription, editLength;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDB = new DatabaseHelper(this); //instantiate the dbhelper class
+
+        editHike = (EditText)findViewById(R.id.editText_hike);
+        editDescription = (EditText)findViewById(R.id.editText_description);
+        editLength = (EditText)findViewById(R.id.editText_length);
+        btnAddData = (Button)findViewById(R.id.button_add);
+        AddData();
+    }
+
+    public void AddData(){
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+
+                    public void onClick(View v) {
+                        boolean isInserted = myDB.insertData(editHike.getText().toString(),
+                                editDescription.getText().toString(),
+                                editLength.getText().toString());
+                        if(isInserted = true)
+                            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Data not Inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
