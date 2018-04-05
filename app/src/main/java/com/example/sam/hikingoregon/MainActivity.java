96f9aab990e2dafee27747918d3f2abf33935a09
Sam Wetzel <samwow13@yahoo.com>
2018-04-04 19:13:34 -0700

    Added api functionality to random hike generator

--- app/src/main/java/com/example/sam/hikingoregon/MainActivity.java
+++ app/src/main/java/com/example/sam/hikingoregon/MainActivity.java
@@ -5,15 +5,29 @@ import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
+import org.xml.sax.XMLReader;
+
+
+import stanford.androidlib.SimpleActivity;
 
 import static android.widget.Toast.makeText;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SimpleActivity {
 
 
     @Override
@@ -34,8 +48,38 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+//    public void goToRandomHike(View view) {
+//        Intent intent = new Intent(this, RandomHikeActivity.class);
+//        startActivity(intent);
+//        Ion.with(this)
+//                .load("https://www.hikingproject.com/data/get-trails?lat=44.9429&lon=123.0351&maxDistance=50&maxResults=1&key=200232475-fb4ba0aa94a3bc700c5cb0b8eb6a9e6e")
+//                .asString()
+//                .setCallback(new FutureCallback<String>() {
+//                    @Override
+//                    public void onCompleted(Exception e, String result) {
+//                        try{
+//                            JSONObject json = XML.toJSONObject(result);
+//
+//                            JSONObject a = json.getJSONObject("trails")
+//                                    .getJSONObject("summary");
+//
+//                            for(int i = 0;i < a.length();i++) {
+//                                JSONObject img = a.getJSONObject(i);
+//                                String url = img.getString("summary");
+//                            }
+//                        }
+//                        catch (JSONException jsone) {
+//                            Log.wtf("help", jsone);
+//                        }
+//                    }
+//                });
+//
+//    }
+
+
     public void goToRandomHike(View view) {
         Intent intent = new Intent(this, RandomHikeActivity.class);
         startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/sam/hikingoregon/RandomHikeActivity.java
+++ app/src/main/java/com/example/sam/hikingoregon/RandomHikeActivity.java
@@ -1,13 +1,73 @@
 package com.example.sam.hikingoregon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class RandomHikeActivity extends AppCompatActivity {
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Random;
+
+import stanford.androidlib.SimpleActivity;
+
+public class RandomHikeActivity extends SimpleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_hike);
+    }
+
+    public void generateRandom(View view) {
+        Ion.with(this)
+                .load("https://www.hikingproject.com/data/get-trails?lat=44.9429&lon=-123.0351&maxDistance=50&maxResults=20&key=200232475-fb4ba0aa94a3bc700c5cb0b8eb6a9e6e")
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        try{
+                           Random rand = new Random();
+                           int value = rand.nextInt(20);
+
+                           JSONObject json = new JSONObject(result); // retrieves the object from input
+                           JSONArray trails = json.getJSONArray("trails"); // gets the array of trails
+                           JSONObject one = trails.getJSONObject(value);// gets the object at value of trail array
+                           String summary = one.getString("summary"); //returns the data for summary
+                           String name = one.getString("name");
+                           String length = one.getString("length");
+                           String stars = one.getString("stars");
+                           String difficulty = one.getString("difficulty");
+                           String location = one.getString("location");
+                           String highElevation = one.getString("high");
+                           String lowElevation = one.getString("low");
+
+
+                           $TV(R.id.hikeName).setText(name);
+                           $TV(R.id.hikeLocation).setText(location);
+                           $TV(R.id.hikeSummary).setText(summary); // prints data for summary to a textview
+                           $TV(R.id.hikeLength).setText(length);
+                           $TV(R.id.hikeStars).setText(stars);
+                           $TV(R.id.hikeDifficulty).setText(difficulty);
+                           $TV(R.id.hikeHighElevation).setText(highElevation);
+                           $TV(R.id.hikeLowElevation).setText(lowElevation);
+
+
+                        }
+                        catch (JSONException jsone) {
+                            Log.wtf("help", jsone);
+                        }
+                    }
+                });
+    }
+
+    public void clearPage(View view) {
+        Intent intent = new Intent(this, RandomHikeActivity.class);
+        startActivity(intent);
     }
 }
