f26f63c8dde4198534c8dea1f86846673a450161
Sam Wetzel <samwow13@yahoo.com>
2018-04-05 18:26:38 -0700

    commented and cleaned code

--- app/src/main/java/com/example/sam/hikingoregon/RandomHikeActivity.java
+++ app/src/main/java/com/example/sam/hikingoregon/RandomHikeActivity.java
@@ -29,23 +29,28 @@ public class RandomHikeActivity extends SimpleActivity {
     }
 
     public void generateRandom(View view) {
+        //resets the picture in random hike view
         GridLayout grid = $(R.id.grid);
         grid.removeAllViews();
 
         Ion.with(this)
+                //this url takes in method parameters, you can check the api for more details.
                 .load("https://www.hikingproject.com/data/get-trails?lat=44.9429&lon=-123.0351&maxDistance=50&maxResults=20&key=200232475-fb4ba0aa94a3bc700c5cb0b8eb6a9e6e")
                 .asString()
                 .setCallback(new FutureCallback<String>() {
                     @Override
                     public void onCompleted(Exception e, String result) {
                         try{
+                           //Random number creation to grab a random item from the array
                            Random rand = new Random();
                            int value = rand.nextInt(20);
 
                            JSONObject json = new JSONObject(result); // retrieves the object from input
                            JSONArray trails = json.getJSONArray("trails"); // gets the array of trails
                            JSONObject one = trails.getJSONObject(value);// gets the object at value of trail array
-                           String summary = one.getString("summary"); //returns the data for summary
+
+                            //returns data for each of the following
+                           String summary = one.getString("summary");
                            String name = one.getString("name");
                            String length = one.getString("length");
                            String stars = one.getString("stars");
@@ -54,20 +59,18 @@ public class RandomHikeActivity extends SimpleActivity {
                            String highElevation = one.getString("high");
                            String lowElevation = one.getString("low");
 
-                           String image = one.getString("imgMedium");
-                           loadImage(image);
+                           String image = one.getString("imgMedium"); // gets the image
+                           loadImage(image); // loads image with method
 
-
+                            //sets textviews to proper inputs
                            $TV(R.id.hikeName).setText(name);
                            $TV(R.id.hikeLocation).setText(location);
-                           $TV(R.id.hikeSummary).setText(summary); // prints data for summary to a textview
+                           $TV(R.id.hikeSummary).setText(summary);
                            $TV(R.id.hikeLength).setText(length);
                            $TV(R.id.hikeStars).setText(stars);
                            $TV(R.id.hikeDifficulty).setText(difficulty);
                            $TV(R.id.hikeHighElevation).setText(highElevation);
                            $TV(R.id.hikeLowElevation).setText(lowElevation);
-
-
                         }
                         catch (JSONException jsone) {
                             Log.wtf("help", jsone);
@@ -76,6 +79,9 @@ public class RandomHikeActivity extends SimpleActivity {
                 });
     }
 
+    /*
+    Method that takes in a url and programmatically exports the image given to a certain location within the random hike.
+     */
     public void loadImage(String url){
         ImageView imgView = new ImageView(this);
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
@@ -91,6 +97,9 @@ public class RandomHikeActivity extends SimpleActivity {
                 .into(imgView);
     }
 
+    /*
+    Method that just reloads the page from scratch.
+     */
     public void clearPage(View view) {
         Intent intent = new Intent(this, RandomHikeActivity.class);
         startActivity(intent);
